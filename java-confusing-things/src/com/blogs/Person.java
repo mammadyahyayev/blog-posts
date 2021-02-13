ebc03f2abb2935bd5bd54d58e41ab131fe2be14a
Mamed Yahyayev <yahyayev2001@gmail.com>
2021-02-13 21:16:33 +0400

    methods of normal class

--- java-confusing-things/src/com/blogs/Person.java
+++ java-confusing-things/src/com/blogs/Person.java
@@ -36,6 +36,36 @@ public class Person { // you can not define private, protected and static class
 
     // public abstract String variable;  // Variable can not be abstract*/
 
+    // Methods of Normal class
+
+    // normal method - this method return String value. Of course it can return any type of value, for example: int, Integer, long , any Class, interface and so on.
+    public String normalMethod(){
+        return "Normal Method";
+    }
+    // void method - this method doesn't return anything because void methods can't return, you can use void methods for printing something or logging and so on.
+    public void voidMethod(){
+        System.out.println("Void Method");
+    }
+
+    // final method - this method return String value. If you create final method in java , you can't override this method from subclass, this can't be overriden.
+    public final String finalMethod(){
+        return "Final Method";
+    }
+
+    // static method - as static variables, you can call this method like this: Classname.methodName. for example: Person.staticMethod.
+    // and also static methods can't be overriden but it can be hidden. And also this method is final.
+    public static String staticMethod(){
+        return "Static Method";
+    }
+
+    // static final method - this method is the same as static method and you don't need to add final keyword to this method because
+    // static methods declared final by default
+    public static final String staticFinalMethod(){
+        return "Static Final Method";
+    }
+
+    // abstract method -- you can't create abstract method in public class, abstract method can be created within the interface and abstract class
+    // public abstract String abstractMethod();
 
     @Override
     public String toString() {
