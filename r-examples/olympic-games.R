a38d796378dd0aafd613db6c06976b9e99d608de
MamedYahyayev <yahyayev2001@gmail.com>
2022-11-24 20:10:36 +0400

    Refactor olympic games script

--- r-examples/olympic-games.R
+++ r-examples/olympic-games.R
@@ -9,25 +9,54 @@ olympic_games <- read_excel("olympic_games.xlsx")
 
 View(olympic_games)
 
-olympic_games <- olympic_games %>%
-  fill(c(Region, Year), .direction = "down")
+olympic_games <- olympic_games %>% 
+  fill(c(Region, Year), .direction="down")
 
-olympic_games_by_region <- olympic_games %>%
-  group_by(Region, Year) %>%
-  summarize()
-
-olympic_games_by_region_count <- olympic_games_by_region %>%
-  group_by(Region) %>%
+olympic_games_by_region <- olympic_games %>% 
+  distinct(Region, Year) %>% 
+  group_by(Region) %>% 
   count()
 
-View(olympic_games_by_region_count)
+View(olympic_games_by_region)
 
-ggplot(data = olympic_games_by_region_count,
-             aes(x = "", y = n, fill = Region)) +
+# Step 1
+ggplot(data = olympic_games_by_region,
+       aes(x="", y = n, fill = Region))
+
+
+# Step 2
+ggplot(data = olympic_games_by_region,
+       aes(x="", y = n, fill = Region)) +
+  geom_bar(stat = "identity", color = "white")
+
+
+# Step 3
+ggplot(data = olympic_games_by_region,
+       aes(x="", y = n, fill = Region)) +
+  geom_bar(stat = "identity", color = "white") +
+  coord_polar("y")
+
+
+# Step 4
+ggplot(data = olympic_games_by_region,
+       aes(x="", y = n, fill = Region)) +
   geom_bar(stat = "identity", color = "white") +
   coord_polar("y") +
   geom_text(aes(label = n),
             position = position_stack(vjust = 0.5),
-            color = "white",
-            size = 5) +
+            color="white",
+            size=5)
+
+
+# Step 5
+ggplot(data = olympic_games_by_region,
+             aes(x="", y = n, fill = Region)) +
+  geom_bar(stat = "identity", color = "white") +
+  coord_polar("y") +
+  geom_text(aes(label = n),
+            position = position_stack(vjust = 0.5),
+            color="white",
+            size=5) +
   labs(title = "The Olympic Games held by regions so far")
+
+    
