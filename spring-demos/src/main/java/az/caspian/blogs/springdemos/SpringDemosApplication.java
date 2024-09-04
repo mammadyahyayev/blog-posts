753c9d660d7589e66be022a592ea30a7c836edbb
Mammad Yahya <mammadyahyayev@gmail.com>
2024-09-04 19:36:09 +0400

    Create spring-demos module for codes in articles that are related to spring

---
+++ spring-demos/src/main/java/az/caspian/blogs/springdemos/SpringDemosApplication.java
@@ -0,0 +1,13 @@
+package az.caspian.blogs.springdemos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemosApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringDemosApplication.class, args);
+  }
+
+}
---
+++ spring-demos/src/test/java/az/caspian/blogs/springdemos/SpringDemosApplicationTests.java
@@ -0,0 +1,13 @@
+package az.caspian.blogs.springdemos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemosApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
