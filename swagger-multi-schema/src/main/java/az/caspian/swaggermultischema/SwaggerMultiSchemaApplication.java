fe192fe980b210f63b2d67db97321c7aeb7e98db
Mammad Yahyayev <mammadyahyayev@gmail.com>
2024-12-23 22:05:36 +0400

    init swagger-multi-schema project

---
+++ swagger-multi-schema/src/main/java/az/caspian/swaggermultischema/SwaggerMultiSchemaApplication.java
@@ -0,0 +1,13 @@
+package az.caspian.swaggermultischema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerMultiSchemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerMultiSchemaApplication.class, args);
+    }
+
+}
---
+++ swagger-multi-schema/src/test/java/az/caspian/swaggermultischema/SwaggerMultiSchemaApplicationTests.java
@@ -0,0 +1,13 @@
+package az.caspian.swaggermultischema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerMultiSchemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
