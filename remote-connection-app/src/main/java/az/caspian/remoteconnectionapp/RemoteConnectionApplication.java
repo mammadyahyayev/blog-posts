e99cfb4db6ee03564a52cc57ec09a01a5516b75e
Mammad Yahyayev <009477@ibar.int>
2025-02-04 19:04:31 +0400

    add remote connection app for debugging remotely from IDE

---
+++ remote-connection-app/src/main/java/az/caspian/remoteconnectionapp/RemoteConnectionApplication.java
@@ -0,0 +1,13 @@
+package az.caspian.remoteconnectionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteConnectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemoteConnectionApplication.class, args);
+    }
+
+}
---
+++ remote-connection-app/src/test/java/az/caspian/remoteconnectionapp/RemoteConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package az.caspian.remoteconnectionapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteConnectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
