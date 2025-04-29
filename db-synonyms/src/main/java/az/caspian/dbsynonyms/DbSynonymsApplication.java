a4545c3942f77aa50f90f63b749a7b5e72cc016d
mammadyahyayev <mammadyahyayev@gmail.com>
2025-04-29 08:51:30 +0400

    init db synonyms project

---
+++ db-synonyms/src/main/java/az/caspian/dbsynonyms/DbSynonymsApplication.java
@@ -0,0 +1,13 @@
+package az.caspian.dbsynonyms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbSynonymsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbSynonymsApplication.class, args);
+    }
+
+}
---
+++ db-synonyms/src/test/java/az/caspian/dbsynonyms/DbSynonymsApplicationTests.java
@@ -0,0 +1,13 @@
+package az.caspian.dbsynonyms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbSynonymsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
